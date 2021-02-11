b11368a55ceb2c3b6146d1e3cfa15e8b7a9899b6
Guillermo Andrés <22777232+gllrm-andres@users.noreply.github.com>
2021-02-10 20:52:52 -0500

    nebular ui added

--- projects/comfeco/src/app/core/core.module.ts
+++ projects/comfeco/src/app/core/core.module.ts
@@ -5,10 +5,19 @@ import { RouterModule } from '@angular/router';
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 import { HeaderComponent } from './layout/main-layout/header/header.component';
 import { FooterComponent } from './layout/main-layout/footer/footer.component';
+import { NbLayoutModule, NbThemeModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
 
 @NgModule({
   declarations: [MainLayoutComponent, HeaderComponent, FooterComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, RouterModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule,
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbThemeModule.forRoot({ name: 'default' }),
+  ],
   exports: [MainLayoutComponent],
 })
 export class CoreModule {}
